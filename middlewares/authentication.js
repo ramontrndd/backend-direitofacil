f9c976975aad85f84d9c55e552d29b813b890bb5
Ramon <ramonbraintrindade@gmail.com>
2024-07-09 18:15:02 -0300

    feat: improviments middlewares

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,15 +3,15 @@ const jwt = require("jsonwebtoken");
 
 function authenticationToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) 
-    return res.status(401).json({ message: "Token não informado" });
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token == null) 
+  return res.status(401);
+    
   
 
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, response) => {
-    if (err) {
-      return res.status(403).json({ message: "Token inválido" });
-    }
+    if (err) 
+      return res.status(403);
     res.locals = response;
     next();
   });
--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -2,11 +2,9 @@ require("dotenv").config();
 
 function checkRole(req, res, next) {
   if (res.locals.role == process.env.USER) {
- res.sendStatus(401).json({message: "Usuário não autorizado"})
-  } else {
+ res.sendStatus(401)
+  } else 
     next();
   }
-}
-
 module.exports = { checkRole: checkRole };
 
