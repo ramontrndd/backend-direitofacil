65937b9618ce92127c238d2a29616a3b527be209
Ramon <ramonbraintrindade@gmail.com>
2024-07-09 16:30:44 -0300

    feat: update checkrole

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 function checkRole(req, res, next) {
-  if (res.locals.role === process.env.USER) {
- return res.status(403).json({ message: "Você não tem permissão para acessar este recurso" });
+  if (res.locals.role == process.env.USER) {
+ res.sendStatus(401).json({message: "Usuário não autorizado"})
   } else {
     next();
   }
 }
 
 module.exports = { checkRole: checkRole };
+
